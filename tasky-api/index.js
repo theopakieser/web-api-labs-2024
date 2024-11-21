e380b7078774ba6d6a85ee52aef679ac494849ae
theopakieser <20101918@mail.wit.ie>
2024-11-21 18:01:57 +0000

    updated index.js

--- tasky-api/index.js
+++ tasky-api/index.js
@@ -3,6 +3,8 @@ import express from 'express';
 import tasksRouter from './api/tasks';
 import './db';
 import usersRouter from './api/users';
+// other imports
+import cors from 'cors';
 
 dotenv.config();
 
@@ -26,6 +28,9 @@ app.use('/api/tasks', tasksRouter);
 app.use(errHandler);
 
 app.use('/api/users', usersRouter);
+
+// Enable CORS for all requests
+app.use(cors());
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
