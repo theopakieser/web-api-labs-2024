5e5d6ab35a993b507fcfde16adfd1765092afb38
theopakieser <20101918@mail.wit.ie>
2024-11-21 17:55:36 +0000

    Mongo lab Users Router

--- tasky-api/index.js
+++ tasky-api/index.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import tasksRouter from './api/tasks';
 import './db';
+import usersRouter from './api/users';
 
 dotenv.config();
 
@@ -23,6 +24,8 @@ app.use(express.json());
 app.use('/api/tasks', tasksRouter);
 
 app.use(errHandler);
+
+app.use('/api/users', usersRouter);
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
