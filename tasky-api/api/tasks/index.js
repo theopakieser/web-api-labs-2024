00ded942340c052547d73be3517768b68b279401
theopakieser <20101918@mail.wit.ie>
2024-11-21 17:49:36 +0000

    Mongo lab Error Handling

--- tasky-api/api/tasks/index.js
+++ tasky-api/api/tasks/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import Task from './taskModel';
+import asyncHandler from 'express-async-handler';
 
 const router = express.Router(); // eslint-disable-line
 
@@ -8,12 +9,11 @@ router.get('/', async (req, res) => {
     const tasks = await Task.find();
     res.status(200).json(tasks);
 });
-
 // create a task
-router.post('/', async (req, res) => {
+router.post('/', asyncHandler(async (req, res) => {
     const task = await Task(req.body).save();
     res.status(201).json(task);
-});
+}));
 // Update Task
 router.put('/:id', async (req, res) => {
     if (req.body._id) delete req.body._id;
