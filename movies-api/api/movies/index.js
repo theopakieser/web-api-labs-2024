735b0f215a686abf88e4e37a99a2ac6ce346105e
theopakieser <20101918@mail.wit.ie>
2024-12-22 19:27:39 +0000

    getMovieDetails API Endpoint

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -5,7 +5,7 @@ import movieModel from './movieModel';
 import asyncHandler from 'express-async-handler';
 import express from 'express';
 import {
-    getNowPlayingMovies, getPopularMovies
+    getNowPlayingMovies, getPopularMovies, getMovieDetails
   } from '../tmdb-api';
 
 
@@ -33,15 +33,15 @@ router.get('/', asyncHandler(async (req, res) => {
 }));
 
 // Get movie details
-router.get('/:id', asyncHandler(async (req, res) => {
-    const id = parseInt(req.params.id);
-    const movie = await movieModel.findByMovieDBId(id);
-    if (movie) {
-        res.status(200).json(movie);
-    } else {
-        res.status(404).json({message: 'The movie you requested could not be found.', status_code: 404});
-    }
-}));
+// router.get('/:id', asyncHandler(async (req, res) => {
+//     const id = parseInt(req.params.id);
+//     const movie = await movieModel.findByMovieDBId(id);
+//     if (movie) {
+//         res.status(200).json(movie);
+//     } else {
+//         res.status(404).json({message: 'The movie you requested could not be found.', status_code: 404});
+//     }
+// }));
 
 router.get('/tmdb/upcoming', asyncHandler(async (req, res) => {
     const upcomingMovies = await getUpcomingMovies();
@@ -61,6 +61,11 @@ router.get('/tmdb/now_playing', asyncHandler(async (req, res) => {
 router.get('/tmdb/popular', asyncHandler(async (req, res) => {
     const popularMovies = await getPopularMovies();
     res.status(200).json(popularMovies);
+}));
+
+router.get('/', asyncHandler(async (req, res) => {
+    const movies = await movieModel.find();
+    res.status(200).json(movies);
 }));
 
 export default router;
--- movies-api/api/tmdb.js
+++ movies-api/api/tmdb.js
@@ -64,3 +64,20 @@ export const getNowPlayingMovies = async () => {
         throw error;
     }
 };
+
+export const getMovieDetails = async () => {
+    try {
+        const response = await fetch(
+            `https://api.themoviedb.org/3/movie/${movieId}?api_key=${process.env.TMDB_KEY}&language=en-US`
+        );
+
+        if (!response.ok) {
+            throw new Error(response.json().message);
+        }
+
+        return await response.json();
+    } catch (error) {
+        throw error;
+    }
+};
+
